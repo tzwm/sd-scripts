eaff11ab6ac198883551af7fe8717f8b730646f1
tzwm <git@tzwm.me>
2023-05-08 11:57:39 +0800

    Add clash config for autodl

--- scripts/init-proxy.sh
+++ scripts/init-proxy.sh
@@ -36,7 +36,23 @@ git config --global http.proxy $proxy
 
 if [ "$1" = "global" ]; then
   echo '开启全局加速'
-  export http_proxy=$proxy
-  export https_proxy=$proxy
-  export no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
+
+  server=$(echo $proxy | sed 's/http:\/\/\([^:]*\):.*/\1/')
+  port=$(echo $proxy | sed 's/.*:\([0-9]*\)$/\1/')
+
+  cd /root/clash
+  sed -i "s/server_address/$server/g" config.yaml
+  sed -i "s/server_port/$port/g" config.yaml
+
+  if pgrep clash > /dev/null
+  then
+    echo "clash is already running"
+  else
+    echo "starting clash"
+    nohup ./clash -f config.yaml &
+  fi
+
+  export http_proxy=http://127.0.0.1:7890
+  export https_proxy=http://127.0.0.1:7890
+  #export no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
 fi
