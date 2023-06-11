c5dd40eb2ff89f4056e59c1f7a06bc07ce42a081
tzwm <git@tzwm.me>
2023-06-11 11:23:22 +0800

    init commit for VisualGLM-6B

--- scripts/post-making-image.sh
+++
@@ -1,8 +0,0 @@
-rm /root/clash/config.yaml
-
-cache_backup_dir="/root/cache"
-cache_dest_dir="/root/.cache"
-mkdir -p $cache_backup_dir
-mv $cache_dest_dir/huggingface $cache_backup_dir/huggingface
-mv $cache_dest_dir/clip $cache_backup_dir/clip
-rm -rf $cache_dest_dir
