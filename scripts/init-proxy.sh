25fc831755728824005a208c468e008a3da64e50
tzwm <git@tzwm.me>
2023-06-27 21:23:32 +0800

    Update proxy by new way

--- scripts/init-proxy.sh
+++ scripts/init-proxy.sh
@@ -1,43 +1,9 @@
 #! /bin/bash
 
-case $AutoDLRegion in
-  #内蒙 A
-  neimeng-A | neimeng-C | neimeng-D)
-    proxy=http://192.168.1.174:12798
-    ;;
-  #北京 A
-  beijing-A | beijing-B | beijing-D | beijing-E)
-    proxy=http://100.72.64.19:12798
-    ;;
-  #芜湖区
-  wuhu-A)
-    proxy=http://192.168.0.91:12798
-    ;;
-  #西北A区
-  west-A)
-    proxy=http://172.18.0.86:12798
-    ;;
-  #佛山区
-  foshan-A)
-    proxy=http://192.168.126.12:12798
-    ;;
-  #毕业
-  stu-A | east-A)
-    proxy=http://10.0.0.7:12798
-    ;;
-  #宿迁企业区
-  suqian-C)
-    proxy=http://10.0.0.7:12798
-    ;;
-  *)
-    echo "不知道你是哪个区的机器"
-    echo "$AutoDLRegion 请把这行发群里，我补上"
-    exit 1
-    ;;
-esac
+bash /etc/network_turbo
 
 echo 'git 科学加速启动'
-git config --global http.proxy $proxy
+git config --global http.proxy $http_proxy
 
 if [ "$1" = "global" ]; then
   echo '开启全局加速'
@@ -51,8 +17,8 @@ if [ "$1" = "global" ]; then
 
     if ! [ -e "config.yaml" ]; then
       cp /root/tzwm_sd_webui_scripts/configs/clash_config.yaml ./config.yaml
-      server=$(echo $proxy | sed 's/http:\/\/\([^:]*\):.*/\1/')
-      port=$(echo $proxy | sed 's/.*:\([0-9]*\)$/\1/')
+      server=$(echo $http_proxy | sed 's/http:\/\/\([^:]*\):.*/\1/')
+      port=$(echo $http_proxy | sed 's/.*:\([0-9]*\)$/\1/')
       sed -i "s/server_address/$server/g" config.yaml
       sed -i "s/server_port/$port/g" config.yaml
     fi
