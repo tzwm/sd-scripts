7f5803a0d498486fa7fd54038c8dbd1054c6abd1
tzwm <git@tzwm.me>
2023-07-02 18:36:52 +0800

    Add lyco-dir to a1111-pack

--- a1111-pack/scripts/restart-webui.sh
+++ a1111-pack/scripts/restart-webui.sh
@@ -8,4 +8,4 @@ echo "重启中，需要等待几秒……"
 
 pgrep -f 'launch.py' | head -n 1 | xargs kill
 
-cd /root/stable-diffusion-webui/ && ./webui.sh -f --port 6006 --enable-insecure-extension-access --api --xformers --opt-sdp-attention --no-half-vae --ckpt-dir /root/autodl-tmp/models/ckpt --embeddings-dir /root/autodl-tmp/models/embeddings --lora-dir /root/autodl-tmp/models/lora --vae-dir /root/autodl-tmp/models/vae --controlnet-dir /root/autodl-tmp/models/controlnet --skip-torch-cuda-test --skip-version-check --skip-python-version-check
+cd /root/stable-diffusion-webui/ && ./webui.sh -f --port 6006 --enable-insecure-extension-access --api --xformers --opt-sdp-attention --no-half-vae --ckpt-dir /root/autodl-tmp/models/ckpt --embeddings-dir /root/autodl-tmp/models/embeddings --lora-dir /root/autodl-tmp/models/lora --vae-dir /root/autodl-tmp/models/vae --controlnet-dir /root/autodl-tmp/models/controlnet --lyco-dir /root/autodl-tmp/models/lycoris --skip-torch-cuda-test --skip-version-check --skip-python-version-check
