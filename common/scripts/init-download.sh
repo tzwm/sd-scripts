e7a5705999095a99cea4248c1b73400f4d2d4f12
tzwm <git@tzwm.me>
2023-07-24 17:38:12 +0800

    Update init-download to remove segment anything and deforum models

--- common/scripts/init-download.sh
+++ common/scripts/init-download.sh
@@ -47,10 +47,10 @@ if [ ! -d "$model_dir/lycoris" ]; then
 fi
 echo "lycoris 搞定"
 
-if [ ! -d "$model_dir/others" ]; then
-  cg down StableDiffusion-others -t $model_dir
-  mv StableDiffusion-others others
-  cd $model_dir/others
+if [ ! -d "$model_dir/others/controlnet_annotator" ]; then
+  mkdir -p others
+  cd others
+  mkdir -p controlnet_annotator
 
   data="controlnet_annotator/leres/res101.pth,res101.pth
 controlnet_annotator/leres/latest_net_G.pth,latest_net_G.pth
@@ -70,35 +70,25 @@ controlnet_annotator/pidinet/table5_pidinet.pth,table5_pidinet.pth
 controlnet_annotator/oneformer/250_16_swin_l_oneformer_ade20k_160k.pth,250_16_swin_l_oneformer_ade20k_160k.pth
 controlnet_annotator/oneformer/150_16_swin_l_oneformer_coco_100ep.pth,150_16_swin_l_oneformer_coco_100ep.pth
 controlnet_annotator/lama/ControlNetLama.pth,ControlNetLama.pth
-controlnet_annotator/uniformer/upernet_global_small.pth,upernet_global_small.pth
-segment_anything/grounding-dino/GroundingDINO_SwinT_OGC.py,GroundingDINO_SwinT_OGC.py
-segment_anything/grounding-dino/groundingdino_swint_ogc.pth,groundingdino_swint_ogc.pth
-segment_anything/sam/PUT_YOUR_SAM_MODEL_HERE.txt,PUT_YOUR_SAM_MODEL_HERE.txt
-segment_anything/sam/sam_vit_h_4b8939.pth,sam_vit_h_4b8939.pth
-Deforum/AdaBins_nyu.pt,AdaBins_nyu.pt
-Deforum/dpt_large-midas-2f21e586.pt,dpt_large-midas-2f21e586.pt"
+controlnet_annotator/uniformer/upernet_global_small.pth,upernet_global_small.pth"
 
   echo "$data" | while read line; do
     target_path=$(echo $line | cut -d ',' -f 1)
     source_path=$(echo $line | cut -d ',' -f 2)
 
+    cg down "StableDiffusion-others/$source_path" -t $model_dir
     mkdir -p $(dirname $target_path)
 
-    mv $source_path $target_path
+    mv "$model_dir/StableDiffusion-others/$source_path" $target_path
   done
 
   rm -r /root/stable-diffusion-webui/extensions/sd-webui-controlnet/annotator/downloads
   ln -s $model_dir/others/controlnet_annotator /root/stable-diffusion-webui/extensions/sd-webui-controlnet/annotator/downloads
 
-  rm -r /root/stable-diffusion-webui/extensions/sd-webui-segment-anything/models
-  ln -s $model_dir/others/segment_anything /root/stable-diffusion-webui/extensions/sd-webui-segment-anything/models
-
-  rm -r /root/stable-diffusion-webui/models/Deforum
-  ln -s $model_dir/others/Deforum /root/stable-diffusion-webui/models/Deforum
+  cd $model_dir
+  rm -r StableDiffusion-others
 fi
 echo "controlnet annotator 搞定"
-echo "segment anything + GroundingDINO 搞定"
-echo "Deforum 搞定"
 
 
 cache_backup_dir="/root/cache"
