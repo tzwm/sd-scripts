9c322a4c2aa9a38456799c3cb28f66cf748e92a0
tzwm <git@tzwm.me>
2023-08-25 20:38:50 +0800

    fix audiocraft init script

--- audiocraft/scripts/init.sh
+++ audiocraft/scripts/init.sh
@@ -3,8 +3,10 @@
 echo "开始初始化……"
 
 if [ -d "/root/cache" ]; then
-  rm -rf "/root/.cache"
-  mv "/root/cache" "/root/.cache"
+  rm -rf "/root/.cache/*"
+  mv "/root/cache/huggingface" "/root/.cache/huggingface"
+  mv "/root/cache/torch" "/root/.cache/torch"
+  mv "/root/cache/matplotlib" "/root/.cache/matplotlib"
 fi
 
 echo "初始化成功"
