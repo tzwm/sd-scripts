33e78ccd48ab30fdbc82c4f298bd98d527511db7
tzwm <git@tzwm.me>
2023-09-03 11:37:20 +0800

    fix(fooocus): cannot open the checkpoints directory in the jupyterlab
    issue: https://github.com/jupyterlab/jupyterlab/issues/12408

--- fooocus/scripts/post-image.sh
+++ fooocus/scripts/post-image.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 source_dir="/root/autodl-tmp/models"
-target_dir="/root/Fooocus/models/"
+target_dir="/root/Fooocus/models"
 
 rm $target_dir/checkpoints
 rm $target_dir/loras
 mkdir $target_dir/checkpoints
 mkdir $target_dir/loras
 
-cp $source_dir/checkpoints/sd_xl_base_1.0_0.9vae.safetensors $target_dir/checkpoints
-cp $source_dir/checkpoints/sd_xl_refiner_1.0_0.9vae.safetensors $target_dir/checkpoints
+cp $source_dir/checkpoint/sd_xl_base_1.0_0.9vae.safetensors $target_dir/checkpoints
+cp $source_dir/checkpoint/sd_xl_refiner_1.0_0.9vae.safetensors $target_dir/checkpoints
 cp $source_dir/loras/sd_xl_offset_example-lora_1.0.safetensors $target_dir/loras
