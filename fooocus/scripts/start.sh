2473ad945c38365d1f7d3afc734e48b68b8fbe01
tzwm <git@tzwm.me>
2023-09-03 10:37:10 +0800

    update init.sh for fooocus

--- fooocus/scripts/start.sh
+++ fooocus/scripts/start.sh
@@ -2,6 +2,10 @@
 
 echo "启动中……"
 
+if [ ! -d "/root/autodl-tmp/outputs" ]; then
+  mkdir -p /root/autodl-tmp/outputs
+fi
+
 source /root/tzwm-autodl/common/scripts/init-proxy.sh && \
 unset http_proxy https_proxy all_proxy && \
 cd /root/Fooocus && source venv/bin/activate && python launch.py --port 6006 --listen 0.0.0.0
