6dcd0df8337fc46cdbf896cfd5650cd95e045ed4
tzwm <git@tzwm.me>
2023-09-03 11:31:22 +0800

    fix(fooocus): wrong path in the init script

--- fooocus/scripts/init.sh
+++ fooocus/scripts/init.sh
@@ -5,14 +5,16 @@ echo "初始化一下"
 target_model_dir="/root/autodl-tmp/models"
 source_model_dir="/root/Fooocus/models"
 
+mkdir -p $target_model_dir
+
 if [ ! -d "$target_model_dir/checkpoints" ]; then
-  mv $source_model_dir/checkpoints $target_model_dir/
-  ln -s $target_model_dir/checkpoints $source_model_dir/
+  mv $source_model_dir/checkpoints $target_model_dir/checkpoints
+  ln -s $target_model_dir/checkpoints $source_model_dir/checkpoints
 fi
 
 if [ ! -d "$target_model_dir/loras" ]; then
-  mv $source_model_dir/loras $target_model_dir/
-  ln -s $target_model_dir/loras $source_model_dir/
+  mv $source_model_dir/loras $target_model_dir/loras
+  ln -s $target_model_dir/loras $source_model_dir/loras
 fi
 
 echo "初始化完成"
