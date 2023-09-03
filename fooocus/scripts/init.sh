b0dba866ad6f58e7ff12eb102fdf45b0ddb299c0
tzwm <git@tzwm.me>
2023-09-03 10:49:30 +0800

    update init.sh again for fooocus

--- fooocus/scripts/init.sh
+++ fooocus/scripts/init.sh
@@ -2,9 +2,17 @@
 
 echo "初始化一下"
 
-if [ ! -d "/root/autodl-tmp/checkpoints" ]; then
-  mv /root/Fooocus/models/checkpoints /root/autodl-tmp/
-  ln -s /root/autodl-tmp/checkpoints /root/Fooocus/models/
+$target_model_dir="/root/autodl-tmp/models"
+$source_model_dir="/root/Fooocus/models"
+
+if [ ! -d "$target_model_dir/checkpoints" ]; then
+  mv $source_model_dir/checkpoints $target_model_dir/
+  ln -s $target_model_dir/checkpoints $source_model_dir/
+fi
+
+if [ ! -d "$target_model_dir/loras" ]; then
+  mv $source_model_dir/loras $target_model_dir/
+  ln -s $target_model_dir/loras $source_model_dir/
 fi
 
 echo "初始化完成"
