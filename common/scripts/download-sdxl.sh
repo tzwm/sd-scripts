5b93b4983330956f60dce072ebc44e65d396e5a8
tzwm <git@tzwm.me>
2023-09-03 19:24:48 +0800

    feat(common): update sdxl download script

--- common/scripts/download-sdxl.sh
+++ common/scripts/download-sdxl.sh
@@ -1,24 +1,30 @@
 #! /bin/bash
 
 model_dir="/root/autodl-tmp/models"
+cg_repo="stable-diffusion-xl-1.0"
 
 echo "下载 SDXL base、refiner、VAE 和 LoRA 模型"
 mkdir -p $model_dir
 cd $model_dir
 
-mkdir -p $model_dir/checkpoint
-cg down tzwm/stable-diffusion-xl-1.0/sd_xl_base_1.0.safetensors -t $model_dir
-cg down tzwm/stable-diffusion-xl-1.0/sd_xl_refiner_1.0.safetensors -t $model_dir
-mv "stable-diffusion-xl-1.0/sd_xl_base_1.0.safetensors" checkpoint/
-mv "stable-diffusion-xl-1.0/sd_xl_refiner_1.0.safetensors" checkpoint/
+function cgsdxl() {
+  model_type=$1
+  filename=$2
 
-mkdir -p $model_dir/vae
-cg down tzwm/stable-diffusion-xl-1.0/sdxl_vae.safetensors -t $model_dir
-mv "stable-diffusion-xl-1.0/sdxl_vae.safetensors" vae/
+  mkdir -p $model_type
+  if [ ! -f "$model_type/$filename" ]; then
+    cg down tzwm/$cg_repo/$filename -t $model_dir
+    mv "$cg_repo/$filename" $model_type/
+  fi
+}
 
-mkdir -p $model_dir/lora
-cg down tzwm/stable-diffusion-xl-1.0/sd_xl_offset_example-lora_1.0.safetensors -t $model_dir
-mv "stable-diffusion-xl-1.0/sd_xl_offset_example-lora_1.0.safetensors" lora/
+cgsdxl "checkpoint" "sd_xl_base_1.0.safetensors"
+cgsdxl "checkpoint" "sd_xl_refiner_1.0.safetensors"
+cgsdxl "vae" "sdxl_vae.safetensors"
+cgsdxl "lora" "sd_xl_offset_example-lora_1.0.safetensors"
 
-rm -r stable-diffusion-xl-1.0
+if [ -d $cg_repo ]; then
+  rm -r $cg_repo
+fi
+
 echo "下载完成，已经放入对应的目录"
