09aac65372018045fe7460182f65b7e3ea163d9f
tzwm <git@tzwm.me>
2023-09-12 15:36:18 +0800

    feat(common): update download script

--- common/scripts/init-download.sh
+++ common/scripts/init-download.sh
@@ -5,157 +5,162 @@ model_dir="/root/autodl-tmp/models"
 mkdir -p $model_dir
 cd $model_dir
 
-if [ "$1" == "checkpoint" ] && [ ! -d "$model_dir/$1" ]; then
-  cg down StableDiffusion-checkpoints/AnythingV5_v5PrtRE.safetensors -t $model_dir
-  cg down tzwm/StableDiffusion-checkpoints/majicmixRealistic_v6.safetensors -t $model_dir
-  mv StableDiffusion-checkpoints $1
+cg upgrade
 
-  echo -e ">>>> checkpoints 搞定\n"
-fi
-
-if [ "$1" == "vae" ] && [ ! -d "$model_dir/$1" ]; then
-  cg down StableDiffusion-VAE/vae-ft-mse-840000-ema-pruned.safetensors -t $model_dir
-  cg down tzwm/StableDiffusion-VAE/anything-v4.0.vae.pt -t $model_dir
-  mv StableDiffusion-VAE $1
-
-  echo -e ">>>> VAE 搞定\n"
-fi
-
-if [ "$1" == "lora" ] && [ ! -d "$model_dir/$1" ]; then
-  cg down StableDiffusion-LoRAs/makimaChainsawMan_offset.safetensors -t $model_dir
-  mv StableDiffusion-LoRAs $1
-
-  echo -e ">>>> lora 搞定\n"
-fi
-
-if [ "$1" == "embeddings" ] && [ ! -d "$model_dir/$1" ]; then
-  cg down StableDiffusion-Embeddings/easy_negative.safetensors -t $model_dir
-  mv StableDiffusion-Embeddings $1
-
-  echo -e ">>>> embeddings 搞定\n"
-fi
-
-if [ "$1" == "controlnet" ] && [ ! -d "$model_dir/$1" ]; then
-  cg down ControlNet-v1-1-diff -t $model_dir
-  mv ControlNet-v1-1-diff $1
-
-  cg down tzwm/ControlNet-others/control_v1p_sd15_brightness.safetensors -t $model_dir
-  mv ControlNet-others/control_v1p_sd15_brightness.safetensors $1/
-  cg down tzwm/ControlNet-others/control_v1p_sd15_illumination.safetensors -t $model_dir
-  mv ControlNet-others/control_v1p_sd15_illumination.safetensors $1/
-  cg down tzwm/ControlNet-others/controlnetQRPatternQR_v2Sd15.safetensors -t $model_dir
-  mv ControlNet-others/controlnetQRPatternQR_v2Sd15.safetensors $1/
-  rm -r ControlNet-others
-
-  echo -e ">>>> controlnet 搞定\n"
-fi
-
-if [ "$1" == "lycoris" ] && [ ! -d "$model_dir/$1" ]; then
-  cg down StableDiffusion-lycoris/InsPX.safetensors -t $model_dir
-  mv StableDiffusion-lycoris $1
-
-  echo -e ">>>> lycoris 搞定\n"
-fi
-
+# model_type, data(dir, cg_name), cg_repo, [optional]target_dir
 function cgdown() {
   mkdir -p $1
 
   local data=$2
+  local target_dir="$1"
+  if [ -n "$4" ]; then
+    target_dir="$4"
+  fi
   echo "$data" | while read line; do
-    target_path=$(echo $line | cut -d ',' -f 1)
+    target_path="$target_dir/$(echo $line | cut -d ',' -f 1)"
     source_path=$(echo $line | cut -d ',' -f 2)
 
-    cg down "StableDiffusion-others/$source_path" -t $model_dir
+    if [ -e "$target_path" ]; then
+      continue
+    fi
+
+    cg down "$3/$source_path" -t $model_dir
     mkdir -p $(dirname $target_path)
 
-    mv "$model_dir/StableDiffusion-others/$source_path" $target_path
+    mv "$model_dir/$3/$source_path" $target_path
   done
 
-  rm -r StableDiffusion-others
+  rm -r $3
 }
 
-if [ "$1" == "controlnet_annotator" ] && [ ! -d "$model_dir/$1" ]; then
-  data="controlnet_annotator/leres/res101.pth,res101.pth
-controlnet_annotator/leres/latest_net_G.pth,latest_net_G.pth
-controlnet_annotator/lineart_anime/netG.pth,netG.pth
-controlnet_annotator/lineart/sk_model.pth,sk_model.pth
-controlnet_annotator/lineart/sk_model2.pth,sk_model2.pth
-controlnet_annotator/midas/dpt_hybrid-midas-501f0c75.pt,dpt_hybrid-midas-501f0c75.pt
-controlnet_annotator/openpose/body_pose_model.pth,body_pose_model.pth
-controlnet_annotator/openpose/hand_pose_model.pth,hand_pose_model.pth
-controlnet_annotator/openpose/facenet.pth,facenet.pth
-controlnet_annotator/openpose/dw-ll_ucoco_384.onnx,dw-ll_ucoco_384.onnx
-controlnet_annotator/openpose/yolox_l.onnx,yolox_l.onnx
-controlnet_annotator/hed/ControlNetHED.pth,ControlNetHED.pth
-controlnet_annotator/zoedepth/ZoeD_M12_N.pt,ZoeD_M12_N.pt
-controlnet_annotator/manga_line/erika.pth,erika.pth
-controlnet_annotator/mlsd/mlsd_large_512_fp32.pth,mlsd_large_512_fp32.pth
-controlnet_annotator/normal_bae/scannet.pt,scannet.pt
-controlnet_annotator/pidinet/table5_pidinet.pth,table5_pidinet.pth
-controlnet_annotator/oneformer/250_16_swin_l_oneformer_ade20k_160k.pth,250_16_swin_l_oneformer_ade20k_160k.pth
-controlnet_annotator/oneformer/150_16_swin_l_oneformer_coco_100ep.pth,150_16_swin_l_oneformer_coco_100ep.pth
-controlnet_annotator/lama/ControlNetLama.pth,ControlNetLama.pth
-controlnet_annotator/uniformer/upernet_global_small.pth,upernet_global_small.pth"
+# input, model_type, data(dir, cg_name), cg_repo
+function check_and_download() {
+  if [ "$1" == "$2" ]; then
+    cgdown "$1" "$3" "$4"
+  fi
 
-  cgdown "$1" "$data"
-
-  rm -r /root/stable-diffusion-webui/extensions/sd-webui-controlnet/annotator/downloads
-  ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/sd-webui-controlnet/annotator/downloads
-
-  echo -e ">>>> ControlNet Annotator 模型搞定\n"
-fi
+  echo -e ">>>> $1 搞定\n"
+}
 
 
-if [ "$1" == "segment_anything" ] && [ ! -d "$model_dir/$1" ]; then
-  data="segment_anything/grounding-dino/GroundingDINO_SwinT_OGC.py,GroundingDINO_SwinT_OGC.py
-segment_anything/grounding-dino/groundingdino_swint_ogc.pth,groundingdino_swint_ogc.pth
-segment_anything/sam/sam_vit_h_4b8939.pth,sam_vit_h_4b8939.pth"
+data="AnythingV5_v5PrtRE.safetensors,AnythingV5_v5PrtRE.safetensors
+majicmixRealistic_v6.safetensors,majicmixRealistic_v6.safetensors"
+check_and_download "$1" "checkpoint" "$data" "StableDiffusion-checkpoints"
 
-  cgdown "$1" "$data"
 
-  rm -r /root/stable-diffusion-webui/extensions/sd-webui-segment-anything/models
-  ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/sd-webui-segment-anything/models
+data="vae-ft-mse-840000-ema-pruned.safetensors,vae-ft-mse-840000-ema-pruned.safetensors
+anything-v4.0.vae.pt,anything-v4.0.vae.pt"
+check_and_download "$1" "vae" "$data" "StableDiffusion-VAE"
 
-  echo -e ">>>> Segment Anything 模型搞定"
-  echo -e ">>>> GroundingDINO 模型搞定\n"
-fi
 
-if [ "$1" == "Deforum" ] && [ ! -d "$model_dir/$1" ]; then
-  data="Deforum/AdaBins_nyu.pt,AdaBins_nyu.pt
-Deforum/dpt_large-midas-2f21e586.pt,dpt_large-midas-2f21e586.pt"
+data="pensketch_lora_v2.3.safetensors,pensketch_lora_v2.3.safetensors"
+check_and_download "$1" "lora" "$data" "StableDiffusion-LoRAs"
 
-  cgdown "$1" "$data"
 
-  rm -r /root/stable-diffusion-webui/models/Deforum
-  ln -s $model_dir/$1 /root/stable-diffusion-webui/models/Deforum
+data="easy_negative.safetensors,easy_negative.safetensors"
+check_and_download "$1" "embeddings" "$data" "StableDiffusion-Embeddings"
 
-  echo -e ">>>> Deforum 模型搞定\n"
-fi
 
-if [ "$1" == "SadTalker" ] && [ ! -d "$model_dir/$1" ]; then
-  data="SadTalker/mapping_00109-model.pth.tar,mapping_00109-model.pth.tar
-SadTalker/mapping_00229-model.pth.tar,mapping_00229-model.pth.tar
-SadTalker/SadTalker_V0.0.2_256.safetensors,SadTalker_V0.0.2_256.safetensors
-SadTalker/SadTalker_V0.0.2_512.safetensors,SadTalker_V0.0.2_512.safetensors"
+#controlnet sd1.5
+data="control_v11f1e_sd15_tile_fp16.safetensors,control_v11f1e_sd15_tile_fp16.safetensors
+control_v11f1p_sd15_depth_fp16.safetensors,control_v11f1p_sd15_depth_fp16.safetensors
+control_v11p_sd15_lineart_fp16.safetensors,control_v11p_sd15_lineart_fp16.safetensors
+control_v11p_sd15_openpose_fp16.safetensors,control_v11p_sd15_openpose_fp16.safetensors
+control_v11p_sd15_inpaint_fp16.safetensors,control_v11p_sd15_inpaint_fp16.safetensors
+control_v11f1e_sd15_tile.yaml,control_v11f1e_sd15_tile.yaml
+control_v11f1p_sd15_depth.yaml,control_v11f1p_sd15_depth.yaml
+control_v11p_sd15_lineart.yaml,control_v11p_sd15_lineart.yaml
+control_v11p_sd15_openpose.yaml,control_v11p_sd15_openpose.yaml
+control_v11p_sd15_inpaint.yaml,control_v11p_sd15_inpaint.yaml"
+#control_v11p_sd15_canny_fp16.safetensors,control_v11p_sd15_canny_fp16.safetensors
+#control_v11e_sd15_ip2p_fp16.safetensors,control_v11e_sd15_ip2p_fp16.safetensors
+#control_v11e_sd15_shuffle_fp16.safetensors,control_v11e_sd15_shuffle_fp16.safetensors
+#control_v11p_sd15_mlsd_fp16.safetensors,control_v11p_sd15_mlsd_fp16.safetensors
+#control_v11p_sd15_normalbae_fp16.safetensors,control_v11p_sd15_normalbae_fp16.safetensors
+#control_v11p_sd15_scribble_fp16.safetensors,control_v11p_sd15_scribble_fp16.safetensors
+#control_v11p_sd15_seg_fp16.safetensors,control_v11p_sd15_seg_fp16.safetensors
+#control_v11p_sd15_softedge_fp16.safetensors,control_v11p_sd15_softedge_fp16.safetensors
+#control_v11p_sd15s2_lineart_anime_fp16.safetensors,control_v11p_sd15s2_lineart_anime_fp16.safetensors
+#control_v11p_sd15_canny.yaml,control_v11p_sd15_canny.yaml
+#control_v11e_sd15_ip2p.yaml,control_v11e_sd15_ip2p.yaml
+#control_v11e_sd15_shuffle.yaml,control_v11e_sd15_shuffle.yaml
+#control_v11p_sd15_mlsd.yaml,control_v11p_sd15_mlsd.yaml
+#control_v11p_sd15_normalbae.yaml,control_v11p_sd15_normalbae.yaml
+#control_v11p_sd15_scribble.yaml,control_v11p_sd15_scribble.yaml
+#control_v11p_sd15_seg.yaml,control_v11p_sd15_seg.yaml
+#control_v11p_sd15_softedge.yaml,control_v11p_sd15_softedge.yaml
+#control_v11p_sd15s2_lineart_anime.yaml,control_v11p_sd15s2_lineart_anime.yaml
+check_and_download "$1" "controlnet_sd15" "$data" "ControlNet-v1-1-diff" "controlnet"
 
-  cgdown "$1" "$data"
+data="control_v1p_sd15_brightness.safetensors,control_v1p_sd15_brightness.safetensors"
+#control_v1p_sd15_illumination.safetensors,control_v1p_sd15_illumination.safetensors
+#controlnetQRPatternQR_v2Sd15.safetensors,controlnetQRPatternQR_v2Sd15.safetensors"
+check_and_download "$1" "controlnet_sd15" "$data" "ControlNet-others" "controlnet"
 
-  rm -r /root/stable-diffusion-webui/extensions/SadTalker/checkpoints
-  ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/SadTalker/checkpoints
+#controlnet sdxl
+data="
+"
+check_and_download "$1" "controlnet_sdxl" "$data" "ControlNet-SDXL" "controlnet"
 
-  echo -e ">>>> SadTalker 模型搞定\n"
-fi
 
-if [ "$1" == "AnimateDiff" ] && [ ! -d "$model_dir/$1" ]; then
-  data="AnimateDiff/mm_sd_v15.ckpt,mm_sd_v15.ckpt"
+data="InsPX.safetensors,InsPX.safetensors"
+check_and_download "$1" "lycoris" "$data" "StableDiffusion-lycoris"
 
-  cgdown "$1" "$data"
 
-  rm -r /root/stable-diffusion-webui/extensions/sd-webui-animatediff/model
-  ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/sd-webui-animatediff/model
+data="leres/res101.pth,res101.pth
+leres/latest_net_G.pth,latest_net_G.pth
+lineart_anime/netG.pth,netG.pth
+lineart/sk_model.pth,sk_model.pth
+lineart/sk_model2.pth,sk_model2.pth
+midas/dpt_hybrid-midas-501f0c75.pt,dpt_hybrid-midas-501f0c75.pt
+openpose/body_pose_model.pth,body_pose_model.pth
+openpose/hand_pose_model.pth,hand_pose_model.pth
+openpose/facenet.pth,facenet.pth
+openpose/dw-ll_ucoco_384.onnx,dw-ll_ucoco_384.onnx
+openpose/yolox_l.onnx,yolox_l.onnx
+hed/ControlNetHED.pth,ControlNetHED.pth
+zoedepth/ZoeD_M12_N.pt,ZoeD_M12_N.pt
+manga_line/erika.pth,erika.pth
+mlsd/mlsd_large_512_fp32.pth,mlsd_large_512_fp32.pth
+normal_bae/scannet.pt,scannet.pt
+pidinet/table5_pidinet.pth,table5_pidinet.pth
+oneformer/250_16_swin_l_oneformer_ade20k_160k.pth,250_16_swin_l_oneformer_ade20k_160k.pth
+oneformer/150_16_swin_l_oneformer_coco_100ep.pth,150_16_swin_l_oneformer_coco_100ep.pth
+lama/ControlNetLama.pth,ControlNetLama.pth
+uniformer/upernet_global_small.pth,upernet_global_small.pth"
+check_and_download "$1" "controlnet_annotator" "$data" "StableDiffusion-others"
+rm -r /root/stable-diffusion-webui/extensions/sd-webui-controlnet/annotator/downloads
+ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/sd-webui-controlnet/annotator/downloads
 
-  echo -e ">>>> AnimateDiff 模型搞定\n"
-fi
+
+data="grounding-dino/GroundingDINO_SwinT_OGC.py,GroundingDINO_SwinT_OGC.py
+grounding-dino/groundingdino_swint_ogc.pth,groundingdino_swint_ogc.pth
+sam/sam_vit_h_4b8939.pth,sam_vit_h_4b8939.pth"
+check_and_download "$1" "segment_anything" "$data" "StableDiffusion-others"
+rm -r /root/stable-diffusion-webui/extensions/sd-webui-segment-anything/models
+ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/sd-webui-segment-anything/models
+
+
+data="AdaBins_nyu.pt,AdaBins_nyu.pt
+dpt_large-midas-2f21e586.pt,dpt_large-midas-2f21e586.pt"
+check_and_download "$1" "Deforum" "$data" "StableDiffusion-others"
+rm -r /root/stable-diffusion-webui/models/Deforum
+ln -s $model_dir/$1 /root/stable-diffusion-webui/models/Deforum
+
+
+data="mapping_00109-model.pth.tar,mapping_00109-model.pth.tar
+mapping_00229-model.pth.tar,mapping_00229-model.pth.tar
+SadTalker_V0.0.2_256.safetensors,SadTalker_V0.0.2_256.safetensors
+SadTalker_V0.0.2_512.safetensors,SadTalker_V0.0.2_512.safetensors"
+check_and_download "$1" "SadTalker" "$data" "StableDiffusion-others"
+rm -r /root/stable-diffusion-webui/extensions/SadTalker/checkpoints
+ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/SadTalker/checkpoints
+
+
+data="mm_sd_v15.ckpt,mm_sd_v15.ckpt"
+check_and_download "$1" "AnimateDiff" "$data" "StableDiffusion-others"
+rm -r /root/stable-diffusion-webui/extensions/sd-webui-animatediff/model
+ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/sd-webui-animatediff/model
 
 
 cache_backup_dir="/root/cache"
