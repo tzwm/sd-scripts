e37fc45056a26ed98ea947c6a419ddc19ff509a7
tzwm <git@tzwm.me>
2023-10-13 21:20:06 +0800

    feat(fooocus): update to v2.1 and fix bugs

--- fooocus/scripts/post-image.sh
+++ fooocus/scripts/post-image.sh
@@ -8,6 +8,6 @@ rm $target_dir/loras
 mkdir $target_dir/checkpoints
 mkdir $target_dir/loras
 
-cp $source_dir/checkpoints/sd_xl_base_1.0_0.9vae.safetensors $target_dir/checkpoints
-cp $source_dir/checkpoints/sd_xl_refiner_1.0_0.9vae.safetensors $target_dir/checkpoints
+cp $source_dir/checkpoint/sd_xl_base_1.0_0.9vae.safetensors $target_dir/checkpoints
+cp $source_dir/checkpoint/sd_xl_refiner_1.0_0.9vae.safetensors $target_dir/checkpoints
 cp $source_dir/loras/sd_xl_offset_example-lora_1.0.safetensors $target_dir/loras
