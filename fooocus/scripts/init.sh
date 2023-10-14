7cb1fa174b8d6f82f1bd7ae99dedbff6a8764eff
tzwm <git@tzwm.me>
2023-10-14 14:44:50 +0800

    feat(fooocus): update init to reduce the size of docker image

--- fooocus/scripts/init.sh
+++ fooocus/scripts/init.sh
@@ -1,30 +1,25 @@
 #! /bin/bash
 
-echo "初始化一下"
+echo "初始化一下，请稍等一下……"
 
 target_model_dir="/root/autodl-tmp/models"
-source_model_dir="/root/Fooocus/models"
 
 mkdir -p $target_model_dir
+cd $target_model_dir
 
 if [ ! -d "$target_model_dir/checkpoint" ]; then
-  mv $source_model_dir/checkpoints $target_model_dir/checkpoint
-  ln -s $target_model_dir/checkpoint $source_model_dir/checkpoints
-
+  cg down tzwm/StableDiffusion-checkpoints/sd_xl_base_1.0_0.9vae.safetensors
+  cg down tzwm/StableDiffusion-checkpoints/sd_xl_refiner_1.0_0.9vae.safetensors
   cg down tzwm/StableDiffusion-checkpoints/realisticStockPhoto_v10.safetensors
   cg down tzwm/StableDiffusion-checkpoints/bluePencilXL_v050.safetensors
   cg down tzwm/StableDiffusion-checkpoints/DreamShaper_8_pruned.safetensors
-  mv StableDiffusion-checkpoints/* $target_model_dir/checkpoint
-  rm -r StableDiffusion-checkpoints
+  mv StableDiffusion-checkpoints $target_model_dir/checkpoint
 fi
 
 if [ ! -d "$target_model_dir/loras" ]; then
-  mv $source_model_dir/loras $target_model_dir/loras
-  ln -s $target_model_dir/loras $source_model_dir/loras
-
+  cg down tzwm/StableDiffusion-LoRAs/sd_xl_offset_example-lora_1.0.safetensors
   cg down tzwm/StableDiffusion-LoRAs/SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors
-  mv StableDiffusion-LoRAs/* $target_model_dir/loras
-  rm -r StableDiffusion-LoRAs
+  mv StableDiffusion-LoRAs $target_model_dir/loras
 fi
 
 echo "初始化完成"
