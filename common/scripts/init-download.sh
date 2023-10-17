38e341d2e9a8dc1eff6c0b29d7767568a2a3473b
tzwm <git@tzwm.me>
2023-10-17 21:16:08 +0800

    feat(common): update animatediff models

--- common/scripts/init-download.sh
+++ common/scripts/init-download.sh
@@ -214,11 +214,19 @@ SadTalker_V0.0.2_512.safetensors,SadTalker_V0.0.2_512.safetensors"
   ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/SadTalker/checkpoints
 fi
 
-if [ "$1" == "AnimateDiff" ]; then
+if [ "$1" == "animatediff_model" ]; then
   data="temporaldiff-v1-animatediff.ckpt,temporaldiff-v1-animatediff.ckpt
 hsxl_temporal_layers.f16.safetensors,hsxl_temporal_layers.f16.safetensors
-mm_sd_v15_v2.ckpt,mm_sd_v15_v2.ckpt
-loras/v2_lora_PanLeft.ckpt,v2_lora_PanLeft.ckpt
+mm_sd_v15_v2.ckpt,mm_sd_v15_v2.ckpt"
+
+  cgdown "$1" "$data" "AnimateDiff-Models"
+
+  #rm -r /root/stable-diffusion-webui/extensions/sd-webui-animatediff/model
+  #ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/sd-webui-animatediff/model
+fi
+
+if [ "$1" == "animatediff_lora" ]; then
+  data="loras/v2_lora_PanLeft.ckpt,v2_lora_PanLeft.ckpt
 loras/v2_lora_PanRight.ckpt,v2_lora_PanRight.ckpt
 loras/v2_lora_RollingAnticlockwise.ckpt,v2_lora_RollingAnticlockwise.ckpt
 loras/v2_lora_RollingClockwise.ckpt,v2_lora_RollingClockwise.ckpt
@@ -228,11 +236,7 @@ loras/v2_lora_ZoomIn.ckpt,v2_lora_ZoomIn.ckpt
 loras/v2_lora_ZoomOut.ckpt,v2_lora_ZoomOut.ckpt"
 
   cgdown "$1" "$data" "AnimateDiff-Models"
-
-  rm -r /root/stable-diffusion-webui/extensions/sd-webui-animatediff/model
-  ln -s $model_dir/$1 /root/stable-diffusion-webui/extensions/sd-webui-animatediff/model
 fi
-
 
 cache_backup_dir="/root/cache"
 cache_dest_dir="/root/.cache"
