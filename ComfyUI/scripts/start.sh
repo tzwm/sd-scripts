04c89c7786a77cf7b6c3721aef70ba1c918fc858
tzwm <git@tzwm.me>
2023-11-17 15:47:37 +0800

    feat(ComfyUI): add annotator init

--- ComfyUI/scripts/start.sh
+++ ComfyUI/scripts/start.sh
@@ -3,7 +3,17 @@
 echo "启动中……"
 
 mkdir -p /root/autodl-tmp/outputs
-cd /root/ComfyUI
 
+annotator_dir="/root/ComfyUI/custom_nodes/comfyui_controlnet_aux/ckpts"
+cd $annotator_dir
+if [ -e "$annotator_dir/.tzwm_init" ]; then
+  if [ -e '/root/autodl-tmp/models/lllyasviel_annotators' ]; then
+    mkdir -p lllyasviel/Annotators
+    mv /root/autodl-tmp/models/lllyasviel_annotators/* lllyasviel/Annotators/
+    echo $(date +%s) > $annotator_dir/.tzwm_init
+  fi
+fi
+
+cd /root/ComfyUI
 source /root/tzwm-autodl/common/scripts/init-proxy.sh && \
 python main.py --port 6006 --listen 0.0.0.0
