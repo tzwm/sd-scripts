380f391f0770a94daad8f02bb62cb6f52689f83a
tzwm <git@tzwm.me>
2024-01-20 00:01:01 +0800

    [fooocus] tiny updates

--- fooocus/scripts/init.sh
+++ fooocus/scripts/init.sh
@@ -8,8 +8,8 @@ mkdir -p $target_model_dir
 cd $target_model_dir
 
 if [ ! -d "$target_model_dir/checkpoint" ]; then
-  cg down tzwm/StableDiffusion-checkpoints/sd_xl_base_1.0_0.9vae.safetensors
-  cg down tzwm/StableDiffusion-checkpoints/sd_xl_refiner_1.0_0.9vae.safetensors
+  #cg down tzwm/StableDiffusion-checkpoints/sd_xl_base_1.0_0.9vae.safetensors
+  #cg down tzwm/StableDiffusion-checkpoints/sd_xl_refiner_1.0_0.9vae.safetensors
   cg down tzwm/StableDiffusion-checkpoints/realisticStockPhoto_v10.safetensors
   cg down tzwm/StableDiffusion-checkpoints/bluePencilXL_v050.safetensors
   cg down tzwm/StableDiffusion-checkpoints/DreamShaper_8_pruned.safetensors
