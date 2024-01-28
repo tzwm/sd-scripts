4d894ba38ee86e7751067dfe2e0caa4d9a17877e
tzwm <git@tzwm.me>
2024-01-28 16:07:00 +0800

    update GPT4V-Image-Captioner

--- GPT4V-Image-Captioner/scripts/init-download.sh
+++ GPT4V-Image-Captioner/scripts/init-download.sh
@@ -6,8 +6,19 @@ cd $model_dir
 
 if [ -e "cogagent-vqa-hf" ]; then
   echo "$model_dir/cogagent-vqa-hf 已存在"
+else
+  echo -e "开始下载，有点大稍等一会...\n"
+  cg down tzwm/cogagent-vqa-hf
+  echo -e "cogagent-vqa-hf 下载完成\n"
 fi
 
-echo "开始下载，有点大稍等一会..."
-cg down tzwm/cogagent-vqa-hf
-echo "cogagent-vqa-hf 下载完成"
+cache_backup_dir="/root/cache"
+cache_dest_dir="/root/.cache"
+if [ -d $cache_backup_dir ]; then
+  mkdir -p $cache_dest_dir
+  mv $cache_backup_dir/huggingface $cache_dest_dir/huggingface
+  mv $cache_backup_dir/torch $cache_dest_dir/torch 2> /dev/null
+  rm -r $cache_backup_dir
+
+  echo -e "初始化完成"
+fi
--- GPT4V-Image-Captioner/scripts/start.sh
+++ GPT4V-Image-Captioner/scripts/start.sh
@@ -3,4 +3,6 @@
 echo "启动中……"
 cd /root/GPT4V-Image-Captioner
 
+source /etc/network_turbo
+
 ./start_linux_mac.sh
