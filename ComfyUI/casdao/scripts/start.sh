41106b694a1f5fcea37039acf96648bef84e090f
tzwm <git@tzwm.me>
2024-03-17 23:14:03 +0800

    update ComfyUI/casdao

--- ComfyUI/casdao/scripts/start.sh
+++ ComfyUI/casdao/scripts/start.sh
@@ -2,11 +2,11 @@
 
 echo "启动中……"
 
-source ./init_env.sh
+source $HOME/tzwm-sd-scripts/ComfyUI/casdao/scripts/init_env.sh
 
 mkdir -p $tzwm_data_dir/outputs
 
 cd $HOME/ComfyUI
 
 source $tzwm_scripts_dir/ComfyUI/casdao/scripts/init_proxy.sh && \
-python main.py --port 6006 --listen 0.0.0.0
+python main.py --port 4444 --listen 0.0.0.0
