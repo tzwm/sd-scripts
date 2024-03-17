8a3e727cca7a45997838133d7d9128df49ba693e
tzwm <git@tzwm.me>
2024-03-17 23:18:15 +0800

    update ComfyUI/casdao/init_donwload

--- ComfyUI/casdao/scripts/init_donwload.sh
+++ ComfyUI/casdao/scripts/init_donwload.sh
@@ -1,1 +1,14 @@
-HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download tzwm/sd-models --revision casdao --local-dir $HOME/fssd/models --cache-dir $HOME/fssd/huggingface --local-dir-use-symlinks False
+target_dir=$HOME/fssd/models
+cache_dir=$HOME/fssd/huggingface
+
+if [ -e $init_file ]; then
+  echo -e "已经初始化过，跳过模型下载\n"
+  exit
+fi
+
+init_file=$target_dir/.tzwm_init_download
+HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download tzwm/sd-models --revision casdao --local-dir $target_dir --cache-dir $cache_dir --local-dir-use-symlinks False
+
+echo $(date +%s) > $init_file
+
+echo -e "初始化完毕\n"
