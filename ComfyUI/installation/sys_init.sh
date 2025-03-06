7f7d1df3f10e3c84dd721db3d9313a52b3df6d64
tzwm <git@tzwm.me>
2025-03-06 18:21:58 +0800

    chore(installation): add huggingface_hub to pip dependencies

--- ComfyUI/installation/sys_init.sh
+++ ComfyUI/installation/sys_init.sh
@@ -13,6 +13,6 @@ $sudo_prefix apt-get install -y \
 
 pip install --upgrade pip
 
-pip install pickleshare codewithgpu jupyterlab-language-pack-zh-CN
+pip install pickleshare codewithgpu jupyterlab-language-pack-zh-CN huggingface_hub
 
 # curl -fsSL https://ollama.com/install.sh | sh
