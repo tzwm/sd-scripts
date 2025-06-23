c0f16996ba0dde0df9b1bf45817db5d232608c33
tzwm <git@tzwm.me>
2025-06-23 23:18:24 +0800

    add more sys_init

--- ComfyUI/installation/sys_init.sh
+++ ComfyUI/installation/sys_init.sh
@@ -9,10 +9,17 @@ $sudo_prefix apt-get update
 $sudo_prefix apt-get install -y \
   libtalloc-dev gcc g++ make cmake \
   iputils-ping tmux tree git wget \
-  aria2 ffmpeg unzip vim htop
+  aria2 ffmpeg unzip vim htop \
+  fonts-noto-cjk
 
 pip install --upgrade pip
 
 pip install pickleshare codewithgpu jupyterlab-language-pack-zh-CN huggingface_hub
 
 # curl -fsSL https://ollama.com/install.sh | sh
+
+# 安装google-chrome
+wget -c "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install
+rm -rf google-chrome-stable_current_amd64.deb
