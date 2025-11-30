65a2bd184a6ccd30d8234e5819692611885ab144
tzwm <git@tzwm.me>
2025-11-30 22:16:11 +0800

    bump autodl comfyui v7.0

--- ComfyUI/autodl/scripts/cg_app_start.sh
+++ ComfyUI/autodl/scripts/cg_app_start.sh
@@ -4,6 +4,7 @@ cd /root/tzwm-scripts
 bash ComfyUI/autodl/scripts/init_comfyui.sh
 
 python common/utils/downloader.py ComfyUI/autodl/configs/init_files_base.yaml
+python common/utils/downloader.py ComfyUI/autodl/configs/init_files_z_image_turbo.yaml
 python common/utils/downloader.py ComfyUI/autodl/configs/init_files_nunchaku_qwen_image.yaml
 python common/utils/downloader.py ComfyUI/autodl/configs/init_files_wan.yaml
 
